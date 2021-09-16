a41f01be894b47cb4fbfa960ee46f8cff608eb3b
Vyctorpierre <vyctorpierre2009@gmail.com>
2021-09-15 22:03:05 -0300

    versão1

---
+++ banu-whatslab6/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Mensagem from './components/Mensagem';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Mensagem  />
+    </div>
+  );
+}
+
+export default App;
